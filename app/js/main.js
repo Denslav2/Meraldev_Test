5042af47c5c0919af954e795583bc56dc302d044
Den_Slav <joomlakop@gmail.com>
2021-07-19 23:48:53 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 $(function(){
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        paddingLeft: "3px",
+        prevArrow: '<img class="arrow-next" src="img/arrow-next.svg" alt="btn-prew">',
+        nextArrow: '<img class="arrow-prev" src="img/arrow-prev.svg" alt="btn-prew">',
+    });
 
-
-
-  
 });
